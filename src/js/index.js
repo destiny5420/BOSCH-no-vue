700fd1f2c775963009709ea048a3d100ca23aafe
paper.mac <paperhsiao@gmail.com>
2020-10-02 15:44:38 +0800

    [Add] 新增一些有的沒的測試使用

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
-import '../style/index.css';
+import '../style/index.scss';
+import '@fortawesome/fontawesome-free/js/all.js';
+import $ from 'jquery';
 
 console.log('this is index.js');
+$(function () {
+  $('#menu').on('click', function () {
+    console.log('menu be click!');
+    $('#menu-window').show();
+  });
+});
