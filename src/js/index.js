ee4f55abc9b9c723fb8bd751af25f0f43fc3c155
paper.mac <paperhsiao@gmail.com>
2020-10-03 08:35:24 +0800

    [Add] scroll magic import 的時後需使用大寫來處理

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,21 @@
 import '../style/index.scss';
 import '@fortawesome/fontawesome-free/js/all.js';
 import $ from 'jquery';
+import ScrollMagic from 'ScrollMagic';
+import 'ScrollMagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
+
+function onInitScrollMagic() {
+  console.log('*** onInitScrollMagic ***');
+  let controller = new ScrollMagic.Controller({});
+
+  new ScrollMagic.Scene({
+    triggerElement: '#btn-about',
+    duration: 400,
+  })
+    // .setVelocity('#btn-about', { opacity: 0 }, { duration: 400 })
+    .addIndicators()
+    .addTo(controller);
+}
 
 function onAwake() {
   console.log('*** onAwake ***');
@@ -13,9 +28,10 @@ function onAwake() {
   $('#menu-window-close').on('click', function () {
     $('#menu-window').hide();
   });
+
+  onInitScrollMagic();
 }
 
-console.log('this is index.js');
 $(function () {
   onAwake();
 });
