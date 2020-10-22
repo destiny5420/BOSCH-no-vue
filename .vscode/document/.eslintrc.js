d647ce449ac73b18e2532f91192f5489e5e3a32c
paper.window <destiny5420@gmail.com>
2020-10-22 10:39:55 +0800

    [Add] 新增Footer

---
+++ .vscode/document/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/essential', '@vue/airbnb'],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': ['error', 'never'],
+    'object-shorthand': [0, 'methods'],
+    'func-names': 0,
+    'no-useless-return': 0,
+    'linebreak-style': 0,
+  },
+};
