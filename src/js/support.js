cde76e1a32318baa1007d5334b3ffc4be82b541d
paper.window <destiny5420@gmail.com>
2020-10-26 18:28:45 +0800

    [Add] 新增head animation

--- src/js/support.js
+++ src/js/support.js
@@ -11,6 +11,7 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 import faq_collections from '../files/jsons/question.json';
 
 // ***** variable *****
+
 function onLoadingData() {
   var template_html =
     "<div class='list'><input name='toggle' type='checkbox'><div name='list-block'><div class='question'><div><div class='tch-b list-symbol'>Q</div><div class='tch-b list-text'>{{question}}</div></div><div class='que-btn'></div></div><div class='answer'><div class='tch-b list-symbol'>A</div><div class='tch-r list-text'>{{answer}}</div></div></div></div>";
@@ -30,12 +31,49 @@ function onLoadingData() {
   }
 }
 
+function onEventBinding() {}
+
+function onGSAP() {
+  console.log('*** onGSAP ***');
+
+  // Regist Plugin
+  gsap.registerPlugin(ScrollTrigger);
+
+  // list head animation
+  let headAnim = gsap.timeline({
+    delay: 1.25,
+    onComplete: () => {
+      console.log('finish');
+    },
+  });
+  headAnim.from('#common', { duration: 1, opacity: 0, x: -100 });
+
+  // list show animation
+  let all_list = $('.lists-container .list');
+  for (let i = 0; i < all_list.length; i++) {
+    gsap
+      .timeline({
+        scrollTrigger: { trigger: all_list[i], start: 'bottom bottom', markers: true },
+      })
+      .from(all_list[i], { opacity: 0, y: 40, duration: 0.55, ease: 'power1.out' });
+  }
+
+  let all_title = $('.lists-container .title');
+  for (let i = 0; i < all_title.length; i++) {
+    gsap
+      .timeline({
+        scrollTrigger: { trigger: all_title[i], start: 'middle bottom', markers: true },
+      })
+      .from(all_title[i], { opacity: 0, y: 40, duration: 0.55, ease: 'power1.out' });
+  }
+}
+
 function onAwake() {
   console.log('*** onAwake ***');
 
   onLoadingData();
-  // onEventBinding();
-  // onGSAP();
+  onEventBinding();
+  onGSAP();
   $('#loading-bar').fadeOut();
 }
 
