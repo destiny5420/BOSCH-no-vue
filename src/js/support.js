408e9fa7e2275d927a55c5633694876d21fbcef0
paper.window <destiny5420@gmail.com>
2020-10-26 17:07:28 +0800

    [Add] 調整support animation

--- src/js/support.js
+++ src/js/support.js
@@ -13,13 +13,13 @@ import faq_collections from '../files/jsons/question.json';
 // ***** variable *****
 function onLoadingData() {
   var list_html =
-    "<div class='list'><div class='question'><div class='tch-b list-symbol'>Q</div><div class='tch-b list-text'>{{question}}</div><div class='que-btn'>X</div></div><div class='answer'><div class='tch-b list-symbol'>A</div><div class='tch-r list-text'>{{answer}}</div></div></div>";
+    "<div class='list'><input name='toggle' type='checkbox'><div name='list-block'><div class='question'><div><div class='tch-b list-symbol'>Q</div><div class='tch-b list-text'>{{question}}</div></div><div class='que-btn'></div></div><div class='answer'><div class='tch-b list-symbol'>A</div><div class='tch-r list-text'>{{answer}}</div></div></div></div>";
 
   for (let i = 0; i < faq_collections.common.length; i++) {
     var current_list = list_html
       .replace('{{question}}', faq_collections.common[i].question)
       .replace('{{answer}}', faq_collections.common[i].answer);
-    $('#common .list-block').append(current_list);
+    $('#common .block').append(current_list);
   }
 }
 
