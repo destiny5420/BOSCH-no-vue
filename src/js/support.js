2c70e49863e52e972e5425916612cc499c35d03e
paper.window <destiny5420@gmail.com>
2020-10-27 08:55:56 +0800

    [Add] 使用js判斷content高度, 進而控制max-height

--- src/js/support.js
+++ src/js/support.js
@@ -11,10 +11,17 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 import faq_collections from '../files/jsons/question.json';
 
 // ***** variable *****
+let faqDatas = {
+  originHeight: 0,
+  toggle: [],
+  showMaxHeight: [],
+};
 
 function onLoadingData() {
+  console.log('*** onLoadingData ***');
+
   var template_html =
-    "<div class='list'><input name='toggle' type='checkbox'><div name='list-block'><div class='question'><div><div class='tch-b list-symbol'>Q</div><div class='tch-b list-text'>{{question}}</div></div><div class='que-btn'></div></div><div class='answer'><div class='tch-b list-symbol'>A</div><div class='tch-r list-text'>{{answer}}</div></div></div></div>";
+    "<div class='list'><div name='list-block'><div class='question'><div><div class='tch-b list-symbol'>Q</div><div class='tch-b list-text'>{{question}}</div></div><div class='que-btn'></div></div><div class='answer'><div class='tch-b list-symbol'>A</div><div class='tch-r list-text'>{{answer}}</div></div></div></div>";
 
   for (let i = 0; i < faq_collections.common.length; i++) {
     var current_list = template_html
@@ -29,9 +36,38 @@ function onLoadingData() {
       .replace('{{answer}}', faq_collections.service[i].answer);
     $('#service .block').append(current_list);
   }
+
+  // Setting originHeight
+  faqDatas['originHeight'] = $('.list')[0].clientHeight;
 }
 
-function onEventBinding() {}
+function onEventBinding() {
+  console.log('*** onEventBinding ***');
+
+  let allList = $('.list');
+  for (let i = 0; i < allList.length; i++) {
+    // setting toggle flag
+    faqDatas.toggle.push(false);
+
+    // setting max-height for show
+    faqDatas.showMaxHeight.push(allList[i].children[0].clientHeight);
+
+    allList[i].addEventListener('click', (e) => {
+      // console.log(e);
+
+      if (faqDatas.toggle[i] === false) {
+        e.target.style.transitionDuration = faqDatas.showMaxHeight[i] >= 200 ? '' : '0.35s';
+        e.target.style.maxHeight = faqDatas.showMaxHeight[i] + 'px';
+      } else {
+        e.target.style.maxHeight = faqDatas['originHeight'] + 'px';
+      }
+
+      faqDatas.toggle[i] = !faqDatas.toggle[i];
+    });
+  }
+
+  // console.log('allList: ', allList);
+}
 
 function onGSAP() {
   console.log('*** onGSAP ***');
@@ -42,9 +78,6 @@ function onGSAP() {
   // list head animation
   let headAnim = gsap.timeline({
     delay: 1.25,
-    onComplete: () => {
-      console.log('finish');
-    },
   });
   headAnim.from('#common', { duration: 1, opacity: 0, x: -100 });
 
