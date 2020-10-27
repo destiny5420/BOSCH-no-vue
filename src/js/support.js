5cc55924899bc0430f29e2724cb3bdfddc0089fb
paper.window <destiny5420@gmail.com>
2020-10-27 10:09:33 +0800

    [Modify] 修正所有動畫表現

--- src/js/support.js
+++ src/js/support.js
@@ -11,15 +11,18 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 import faq_collections from '../files/jsons/question.json';
 
 // ***** variable *****
+let tmpAnim = null;
 let faqDatas = {
   originHeight: 0,
   toggle: [],
   showMaxHeight: [],
+  clickAnim: [],
 };
 
 function onLoadingData() {
   console.log('*** onLoadingData ***');
 
+  // setting template append result to DOM
   var template_html =
     "<div class='list'><div name='list-block'><div class='question'><div><div class='tch-b list-symbol'>Q</div><div class='tch-b list-text'>{{question}}</div></div><div class='que-btn'></div></div><div class='answer'><div class='tch-b list-symbol'>A</div><div class='tch-r list-text'>{{answer}}</div></div></div></div>";
 
@@ -37,7 +40,7 @@ function onLoadingData() {
     $('#service .block').append(current_list);
   }
 
-  // Setting originHeight
+  // setting originHeight
   faqDatas['originHeight'] = $('.list')[0].clientHeight;
 }
 
@@ -53,13 +56,17 @@ function onEventBinding() {
     faqDatas.showMaxHeight.push(allList[i].children[0].clientHeight);
 
     allList[i].addEventListener('click', (e) => {
-      // console.log(e);
+      console.log(e);
 
       if (faqDatas.toggle[i] === false) {
         e.target.style.transitionDuration = faqDatas.showMaxHeight[i] >= 200 ? '' : '0.35s';
         e.target.style.maxHeight = faqDatas.showMaxHeight[i] + 'px';
+        e.target.children[0].children[0].classList.add('show');
+        faqDatas.clickAnim[i].play();
       } else {
         e.target.style.maxHeight = faqDatas['originHeight'] + 'px';
+        e.target.children[0].children[0].classList.remove('show');
+        faqDatas.clickAnim[i].reverse();
       }
 
       faqDatas.toggle[i] = !faqDatas.toggle[i];
@@ -79,26 +86,41 @@ function onGSAP() {
   let headAnim = gsap.timeline({
     delay: 1.25,
   });
-  headAnim.from('#common', { duration: 1, opacity: 0, x: -100 });
+  headAnim
+    .from('#common', { duration: 1, opacity: 0, x: -100 })
+    .from('#service', { duration: 1, opacity: 0, x: -100 }, '-=1')
+    .from('#shopinfo', { duration: 1, opacity: 0, x: -100 }, '-=1');
 
   // list show animation
   let all_list = $('.lists-container .list');
   for (let i = 0; i < all_list.length; i++) {
+    // setting trigger anim
     gsap
       .timeline({
-        scrollTrigger: { trigger: all_list[i], start: 'bottom bottom', markers: true },
+        scrollTrigger: { trigger: all_list[i], start: 'bottom bottom' },
       })
       .from(all_list[i], { opacity: 0, y: 40, duration: 0.55, ease: 'power1.out' });
+
+    faqDatas.clickAnim.push(
+      gsap
+        .timeline({ paused: true })
+        .to(all_list[i].children[0], { duration: 0.45, opacity: 1, ease: 'power1.inOut' }),
+    );
   }
 
   let all_title = $('.lists-container .title');
   for (let i = 0; i < all_title.length; i++) {
     gsap
       .timeline({
-        scrollTrigger: { trigger: all_title[i], start: 'middle bottom', markers: true },
+        scrollTrigger: { trigger: all_title[i], start: 'middle bottom' },
       })
-      .from(all_title[i], { opacity: 0, y: 40, duration: 0.55, ease: 'power1.out' });
+      .from(all_title[i], { opacity: 0, y: 60, duration: 0.75, ease: 'power1.out' });
   }
+
+  console.log('all_list: ', all_list);
+  // list click animation
+  tmpAnim = gsap.timeline({ paused: true });
+  tmpAnim.to($('div[name="list-block"]')[0], { duration: 0.35, opacity: 1, ease: 'power1.out' });
 }
 
 function onAwake() {
