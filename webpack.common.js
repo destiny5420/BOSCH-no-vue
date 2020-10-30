317ba457e10fcfdf01b6d5b78956d9da76a6b6a3
paper.window <destiny5420@gmail.com>
2020-10-30 17:41:51 +0800

    [Modify] Build File

--- webpack.common.js
+++ webpack.common.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, './docs'),
-    filename: 'js/[name]201030-01.bundle.js',
+    filename: 'js/[name]201030-02.bundle.js',
   },
   module: {
     rules: [
