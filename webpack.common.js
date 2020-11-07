d7d44c750046d926591578f800fa03a5a1a3fe80
paper.window <destiny5420@gmail.com>
2020-11-07 12:38:29 +0800

    [Modify] 調整webpack.common.js裡面的devServer設定, 將contentBase的路徑修正 devServer為再執行npm run serve所執行的暫時伺服器, 因此contentBase為暫時伺服器所參考的root資料夾為何, 這邊將./dist改為./docs *note: 這會跟import時候所寫的路徑資料夾有關

--- webpack.common.js
+++ webpack.common.js
@@ -46,7 +46,7 @@ module.exports = {
     ],
   },
   devServer: {
-    contentBase: path.join(__dirname, './dist'),
+    contentBase: path.join(__dirname, './docs'),
     hot: true,
     port: '8092',
     inline: true,
